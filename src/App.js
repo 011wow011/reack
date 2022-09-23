c13c7f6f6b266647126ccd6afe459b52c94d9480
DESKTOP-7MV8AF5\Snooks <011wow011@naver.com>
2022-09-24 03:17:00 +0900

    state 변경 state는 변경시 바로 재랜더링 되는 이점이있음

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강동의 호랑이'; 
-  
+  let [글제목, b] = useState(['남자 코트 추천','강남 우동맛집','파이썬독학']);
+  let num = [1, 2];
+
+  let [a, c] = [1, 2];
+
+  // let a = num[0];
+  // let c = num[1];
+
+
   return (
     <div className="App">
       <div className="black-nav">
         <h4 style={ {color : 'red', fontSize : '26px'} }>블로그 테스트</h4>
       </div>
       <h4>{ post }</h4>
+      <div className="list">
+        <h4>{ 글제목[0] }</h4>
+        <p>5월 14일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>5월 14일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>5월 14일 발행</p>
+      </div>
+      
     </div>
   );
 }
